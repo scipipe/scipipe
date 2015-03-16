4b58f4b746e8ff6948bd75763a183f0d79d40aa8
Samuel Lampa <samuel.lampa@gmail.com>
2015-03-16 13:30:58 +0000

    Started working on example 02

---
+++ examples/ex02multifile/ex01shell.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	sci "github.com/samuell/scipipe"
+)
+
+func main() {
+	fooWriter := sci.Sh("echo foo > {o:foo1}")
+	fooWriter.OutPathFuncs["foo1"] = func() string {
+		return "foo.txt"
+	}
+
+	barReplacer := sci.Sh("sed 's/foo/bar/g' {i:foo2} > {o:bar}")
+	barReplacer.OutPathFuncs["bar"] = func() string {
+		return barReplacer.GetInPath("foo2") + ".bar"
+	}
+	barReplacer.InPorts["foo2"] = fooWriter.OutPorts["foo1"]
+
+	fooWriter.Init()
+	barReplacer.Init()
+
+	<-barReplacer.OutPorts["bar"]
+}
