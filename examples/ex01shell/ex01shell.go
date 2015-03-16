e24299780ce4f7780099e7fb13742b566438c2f4
Samuel Lampa <samuel.lampa@gmail.com>
2015-03-16 13:32:38 +0000

    More comments in examples

--- examples/ex01shell/ex01shell.go
+++ examples/ex01shell/ex01shell.go
@@ -5,19 +5,27 @@ import (
 )
 
 func main() {
+	// Init fooWriter task
 	fooWriter := sci.Sh("echo foo > {o:foo1}")
+	// Init function for generating output file pattern
 	fooWriter.OutPathFuncs["foo1"] = func() string {
 		return "foo.txt"
 	}
 
+	// Init barReplacer task
 	barReplacer := sci.Sh("sed 's/foo/bar/g' {i:foo2} > {o:bar}")
+	// Init function for generating output file pattern
 	barReplacer.OutPathFuncs["bar"] = func() string {
 		return barReplacer.GetInPath("foo2") + ".bar"
 	}
+
+	// Connect network
 	barReplacer.InPorts["foo2"] = fooWriter.OutPorts["foo1"]
 
+	// Set up tasks for execution
 	fooWriter.Init()
 	barReplacer.Init()
 
+	// Start execution by reading on last port
 	<-barReplacer.OutPorts["bar"]
 }
