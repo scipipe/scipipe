4b9eec64fc4cfbc4487d955045956a901425c30e
Samuel Lampa <samuel.lampa@gmail.com>
2015-03-16 14:36:32 +0000

    Improve comments in example 2

--- examples/ex02multifile/ex02multifile.go
+++ examples/ex02multifile/ex02multifile.go
@@ -16,12 +16,16 @@ func main() {
 	// Set up tasks for execution
 	barReplacer.Init()
 
-	// Connect network
+	// Manually send file targets on the inport of barReplacer
 	for _, name := range []string{"foo1", "foo2", "foo3"} {
 		barReplacer.InPorts["foo2"] <- sci.NewFileTarget(name + ".txt")
 	}
+	// We have to manually close the inport as well here, to
+	// signal that we are done sending targets (the tasks outport will
+	// then automatically be closed as well)
 	close(barReplacer.InPorts["foo2"])
+
 	for f := range barReplacer.OutPorts["bar"] {
-		fmt.Println("Processed file", f.GetPath(), "...")
+		fmt.Println("Finished processing file", f.GetPath(), "...")
 	}
 }
