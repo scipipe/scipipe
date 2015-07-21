e425b5040651c4f70c749e23a3c6fd8cd1e35318
Samuel Lampa <samuel.lampa@gmail.com>
2015-07-21 11:53:28 +0000

    More methods in FileTarget

--- file.go
+++ file.go
@@ -24,8 +24,26 @@ func (ft *FileTarget) GetTempPath() string {
 	return ft.path + ".tmp"
 }
 
+func (ft *FileTarget) Open() *os.File {
+	f, err := os.Open(ft.GetPath())
+	Check(err)
+	return f
+}
+
+func (ft *FileTarget) Read() []byte {
+	dat, err := ioutil.ReadFile(ft.GetPath())
+	Check(err)
+	return dat
+}
+
+func (ft *FileTarget) Write(dat []byte) {
+	err := ioutil.WriteFile(ft.GetTempPath(), dat, 0644)
+	ft.Atomize()
+	Check(err)
+}
+
 func (ft *FileTarget) Atomize() {
-	time.Sleep(1 * time.Second) // TODO: Remove in production. Just for demo purposes!
+	time.Sleep(1 * time.Millisecond) // TODO: Remove in production. Just for demo purposes!
 	err := os.Rename(ft.GetTempPath(), ft.path)
 	Check(err)
 }
