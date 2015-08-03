ef2de0617c99870a38466c9797e5ed82be238a2b
Samuel Lampa <samuel.lampa@gmail.com>
2015-08-03 22:39:35 +0000

    Updating various examples to new OutPathFunc API

--- examples/ex02filequeue/ex02filequeue.go
+++ examples/ex02filequeue/ex02filequeue.go
@@ -9,7 +9,7 @@ func main() {
 
 	fq := sci.FQ("hej1.txt", "hej2.txt", "hej3.txt")
 	fw := sci.Sh("echo {i:in} > {o:out}")
-	fw.OutPathFuncs["out"] = func() string { return fw.GetInPath("in") }
+	fw.OutPathFuncs["out"] = func(t *sci.ShellTask) string { return t.GetInPath("in") }
 	sn := sci.NewSink()
 
 	fw.InPorts["in"] = fq.Out
