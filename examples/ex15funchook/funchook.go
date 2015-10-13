0d26db031668d84813b779eabe00b835399404aa
Samuel Lampa <samuel.lampa@gmail.com>
2015-10-13 16:10:38 +0200

    Add example for the func hook

---
+++ examples/ex15funchook/funchook.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	sci "github.com/samuell/scipipe"
+	str "strings"
+)
+
+func main() {
+	sci.InitLogDebug()
+
+	foo := sci.Shell("{o:foo}")
+	foo.PathGen["foo"] = func(t *sci.ShellTask) string {
+		return "foo.txt"
+	}
+	foo.RunHook = func(t *sci.ShellTask) {
+		outf := t.OutTargets["foo"]
+		outf.Write([]byte("foo"))
+	}
+
+	f2b := sci.Shell("{i:foo}{o:bar}")
+	f2b.PathGen["bar"] = func(t *sci.ShellTask) string {
+		return t.InTargets["foo"].GetPath() + ".bar"
+	}
+	f2b.RunHook = func(t *sci.ShellTask) {
+		text := t.InTargets["foo"].Read()
+		newText := str.Replace(string(text), "foo", "bar", 1)
+		t.OutTargets["bar"].Write([]byte(newText))
+	}
+
+	// Connect stuff
+	f2b.InPorts["foo"] = foo.OutPorts["foo"]
+
+	// Run Pipeline
+	pln := sci.NewPipeline()
+	pln.AddProcs(foo, f2b)
+	pln.Run()
+}
