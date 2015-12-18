648c37b49ce6dceb49300c524c7b192c5c970e67
Samuel Lampa <samuel.lampa@gmail.com>
2015-12-18 15:25:46 +0100

    Test more file path methods

--- file_test.go
+++ file_test.go
@@ -1,6 +1,7 @@
 package scipipe
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -8,10 +9,13 @@ const (
 	TESTPATH = "somepath.txt"
 )
 
-func TestFileTargetPath(t *testing.T) {
+func TestFileTargetPaths(t *testing.T) {
 	ft := NewFileTarget(TESTPATH)
-	path := ft.GetPath()
-	if path != TESTPATH {
-		t.Errorf("Path not properly initialized! (Was", path, "but should be", TESTPATH)
-	}
+	assertPathsEqual(t, ft.GetPath(), TESTPATH)
+	assertPathsEqual(t, ft.GetTempPath(), TESTPATH+".tmp")
+	assertPathsEqual(t, ft.GetFifoPath(), TESTPATH+".fifo")
+}
+
+func assertPathsEqual(t *testing.T, path1 string, path2 string) {
+	assert.Equal(t, path1, path2, "Wrong path returned! (Was", path1, "but should be", path2, ")")
 }
