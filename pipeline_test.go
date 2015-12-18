bd35a039f5e280272654a6ea7b491f323970c0e8
Samuel Lampa <samuel.lampa@gmail.com>
2015-12-18 15:04:11 +0100

    Adding test of pipeline.Run()

--- pipeline_test.go
+++ pipeline_test.go
@@ -6,6 +6,8 @@ import (
 )
 
 func TestAddProcesses(t *t.T) {
+	InitLogError()
+
 	proc1 := NewBogusProcess()
 	proc2 := NewBogusProcess()
 	pipeline := NewPipeline()
@@ -13,16 +15,35 @@ func TestAddProcesses(t *t.T) {
 
 	assert.EqualValues(t, len(pipeline.processes), 2)
 
-	assert.IsType(t, &BogusProcess{}, pipeline.processes[0])
-	assert.IsType(t, &BogusProcess{}, pipeline.processes[1])
+	assert.IsType(t, &BogusProcess{}, pipeline.processes[0], "Process 1 was not of the right type!")
+	assert.IsType(t, &BogusProcess{}, pipeline.processes[1], "Process 2 was not of the right type!")
+}
+
+func TestRunProcessesInPipeline(t *t.T) {
+	proc1 := NewBogusProcess()
+	proc2 := NewBogusProcess()
+
+	pipeline := NewPipeline()
+	pipeline.AddProcesses(proc1, proc2)
+	pipeline.Run()
+
+	// Only the last process is supposed to be run by the pipeline directly,
+	// while the others are only run if an output is pulled on an out-port,
+	// but since we haven't connected the tasks here, only the last one
+	// should be ran in this case.
+	assert.False(t, proc1.WasRun, "Process 1 was run!")
+	assert.True(t, proc2.WasRun, "Process 2 was not run!")
 }
 
 type BogusProcess struct {
-	process
+	BaseProcess
+	WasRun bool
 }
 
 func NewBogusProcess() *BogusProcess {
-	return &BogusProcess{}
+	return &BogusProcess{WasRun: false}
 }
 
-func (t *BogusProcess) Run() {}
+func (p *BogusProcess) Run() {
+	p.WasRun = true
+}
