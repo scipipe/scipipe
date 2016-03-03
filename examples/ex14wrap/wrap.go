81fb58ea9920e03f6b57c56fedd6c32327d56f3e
Samuel Lampa <samuel.lampa@gmail.com>
2016-03-03 21:46:32 +0100

    Completing example 14 of wrapping shell tasks in outer structs

--- examples/ex14wrap/wrap.go
+++ examples/ex14wrap/wrap.go
@@ -1,21 +1,29 @@
-// Example showing how you can wrap shell tasks in "static" tasks (with proper struct fields)
-
 package main
 
 import (
-	"fmt"
 	sci "github.com/samuell/scipipe"
 )
 
 func main() {
-	fmt.Println("Starting ....")
-	f := NewFooer()
-	go f.Run()
-	<-f.OutFoo
-	fmt.Println("Done!")
+	sci.InitLogInfo()
+
+	foo := NewFooer()
+	f2b := NewFoo2Barer()
+	snk := sci.NewSink()
+
+	f2b.InFoo = foo.OutFoo
+	snk.In = f2b.OutBar
+
+	pl := sci.NewPipeline()
+	pl.AddProcs(foo, f2b, snk)
+	pl.Run()
 }
 
+// ------------------------------------------------------------------------
 // Components
+// ------------------------------------------------------------------------
+
+// Fooer
 
 type Fooer struct {
 	InnerProc *sci.ShellProcess
@@ -32,5 +40,30 @@ func NewFooer() *Fooer {
 }
 
 func (p *Fooer) Run() {
+	p.InnerProc.OutPorts["foo"] = p.OutFoo
+	p.InnerProc.Run()
+}
+
+// Foo2Barer
+
+type Foo2Barer struct {
+	InnerProc *sci.ShellProcess
+	InFoo     chan *sci.FileTarget
+	OutBar    chan *sci.FileTarget
+}
+
+func NewFoo2Barer() *Foo2Barer {
+	innerFoo2Bar := sci.Shell("sed 's/foo/bar/g' {i:foo} > {o:bar}")
+	innerFoo2Bar.SetPathFormatterExtend("bar", "foo", ".bar.txt")
+	return &Foo2Barer{
+		InnerProc: innerFoo2Bar,
+		InFoo:     innerFoo2Bar.InPorts["foo"],
+		OutBar:    innerFoo2Bar.OutPorts["bar"],
+	}
+}
+
+func (p *Foo2Barer) Run() {
+	p.InnerProc.InPorts["foo"] = p.InFoo
+	p.InnerProc.OutPorts["bar"] = p.OutBar
 	p.InnerProc.Run()
 }
