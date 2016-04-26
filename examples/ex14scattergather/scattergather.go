af0fd722b6cceadc678d14cdbb286d13a83f6d65
Samuel Lampa <samuel.lampa@gmail.com>
2016-04-26 16:52:55 +0200

    Improved code formatting in scattergather example

--- examples/ex14scattergather/scattergather.go
+++ examples/ex14scattergather/scattergather.go
@@ -1,15 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/samuell/scipipe"
 )
 
 func main() {
-	//scipipe.InitLogDebug()
-
-	// ------------------------------------------------------------------------
-	// INITIALIZE TASKS
-	// ------------------------------------------------------------------------
+	// === INITIALIZE TASKS =======================================================================
 
 	// Download a zipped Chromosome Y fasta file
 	fastaURL := "ftp://ftp.ensembl.org/pub/release-84/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.Y.fa.gz"
@@ -28,53 +26,49 @@ func main() {
 	// file target to two downstream processes
 	dupl := scipipe.NewDuplicator()
 
-	// Count lines in the fasta file
-	gccnt := scipipe.Shell("gccount", "cat {i:infile} | fold -w 1 | grep '[GC]' | wc -l | awk '{ print $1 }' > {o:gccount}")
+	// Count GC & AT characters in the fasta file
+	charCountCommand := "cat {i:infile} | fold -w 1 | grep '[%s]' | wc -l | awk '{ print $1 }' > {o:%s}"
+	gccnt := scipipe.Shell("gccount", fmt.Sprintf(charCountCommand, "GC", "gccount"))
 	gccnt.SetPathFormatExtend("infile", "gccount", ".gccnt")
-	gccat := scipipe.NewConcatenator("gccounts.txt")
-	gcsum := scipipe.Shell("gcsum", "awk '{ SUM += $1 } END { print SUM }' {i:in} > {o:sum}")
-	gcsum.SetPathFormatExtend("in", "sum", ".sum")
-
-	atcnt := scipipe.Shell("atcount", "cat {i:infile} | fold -w 1 | grep '[AT]' | wc -l | awk '{ print $1 }' > {o:atcount}")
+	atcnt := scipipe.Shell("atcount", fmt.Sprintf(charCountCommand, "AT", "atcount"))
 	atcnt.SetPathFormatExtend("infile", "atcount", ".atcnt")
+
+	// Concatenate GC & AT counts
+	gccat := scipipe.NewConcatenator("gccounts.txt")
 	atcat := scipipe.NewConcatenator("atcounts.txt")
-	atsum := scipipe.Shell("atsum", "awk '{ SUM += $1 } END { print SUM }' {i:in} > {o:sum}")
+
+	// Sum up the GC & AT counts on the concatenated file
+	sumCommand := "awk '{ SUM += $1 } END { print SUM }' {i:in} > {o:sum}"
+	gcsum := scipipe.Shell("gcsum", sumCommand)
+	gcsum.SetPathFormatExtend("in", "sum", ".sum")
+	atsum := scipipe.Shell("atsum", sumCommand)
 	atsum.SetPathFormatExtend("in", "sum", ".sum")
 
+	// Finally, calculate the ratio between GC chars, vs. GC+AT chars
 	gcrat := scipipe.Shell("gcratio", "gc=$(cat {i:gcsum}); at=$(cat {i:atsum}); calc \"$gc/($gc+$at)\" > {o:gcratio}")
 	gcrat.SetPathFormatStatic("gcratio", "gcratio.txt")
 
+	// A sink, to drive the network
 	asink := scipipe.NewSink()
 
-	// ------------------------------------------------------------------------
-	// CONNECT DEPENDENCIES
-	// ------------------------------------------------------------------------
+	// === CONNECT DEPENDENCIES ===================================================================
 
 	unzip.InPorts["gzipped"] = wget.OutPorts["chry_zipped"]
 	split.InFile = unzip.OutPorts["ungzipped"]
-
 	dupl.InFile = split.OutSplitFile
-
 	gccnt.InPorts["infile"] = dupl.OutFile1
 	atcnt.InPorts["infile"] = dupl.OutFile2
-
 	gccat.In = gccnt.OutPorts["gccount"]
 	atcat.In = atcnt.OutPorts["atcount"]
-
 	gcsum.InPorts["in"] = gccat.Out
 	atsum.InPorts["in"] = atcat.Out
-
 	gcrat.InPorts["gcsum"] = gcsum.OutPorts["sum"]
 	gcrat.InPorts["atsum"] = atsum.OutPorts["sum"]
-
 	asink.In = gcrat.OutPorts["gcratio"]
+
+	// === RUN PIPELINE ===========================================================================
 
 	piperunner := scipipe.NewPipelineRunner()
 	piperunner.AddProcesses(wget, unzip, split, dupl, gccnt, atcnt, gccat, atcat, gcsum, atsum, gcrat, asink)
-
-	// ------------------------------------------------------------------------
-	// RUN PIPELINE
-	// ------------------------------------------------------------------------
-
 	piperunner.Run()
 }
