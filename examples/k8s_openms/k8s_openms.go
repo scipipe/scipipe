daea778bf9dcb717a16347cb845e41ddf87a140d
Samuel Lampa <samuel.lampa@gmail.com>
2017-02-27 19:41:12 +0100

    Improving K8s OpenMS workflow

--- examples/k8s_openms/k8s_openms.go
+++ examples/k8s_openms/k8s_openms.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"path/filepath"
 	str "strings"
 
 	sp "github.com/scipipe/scipipe"
@@ -13,14 +14,22 @@ const (
 func main() {
 	prun := sp.NewPipelineRunner()
 
-	peakPicker := sp.NewFromShell("PeakPicker", "PeakPickerHiRes -in {i:sample} -out {o:out} -ini {p:ini}")
+	sampleFilesSender := sp.NewIPQueue(workDir + "002_CRa_H9M5_M470_Pool_01_alternate_neg_low_mr.mzML")
+	prun.AddProcess(sampleFilesSender)
+
+	peakPicker := sp.NewFromShell("PeakPicker", "PeakPickerHiRes -in {i:sample} -out {o:out} -ini "+workDir+"openms-params/PPparam.ini")
 	peakPicker.PathFormatters["out"] = func(t *sp.SciTask) string {
-		parts := str.Split(t.GetInPath("sample"), ".")
-		outPath := "results/" + str.Join(parts[:len(parts)-1], "_") + ".peaks"
+		parts := str.Split(filepath.Base(t.GetInPath("sample")), ".")
+		outPath := workDir + "results/" + str.Join(parts[:len(parts)-1], "_") + ".peaks"
 		return outPath
 	}
-
 	prun.AddProcess(peakPicker)
+
+	sink := sp.NewSink()
+	prun.AddProcess(sink)
+
+	peakPicker.In["sample"].Connect(sampleFilesSender.Out)
+	sink.Connect(peakPicker.Out["out"])
 
 	prun.Run()
 }
