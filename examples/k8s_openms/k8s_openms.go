acf1080381ec1f24d45bbb933ad233e33bdc045c
Samuel Lampa <samuel.lampa@gmail.com>
2017-02-27 20:05:00 +0100

    Names must be lowercase in kubernetes

--- examples/k8s_openms/k8s_openms.go
+++ examples/k8s_openms/k8s_openms.go
@@ -17,7 +17,7 @@ func main() {
 	sampleFilesSender := sp.NewIPQueue(workDir + "002_CRa_H9M5_M470_Pool_01_alternate_neg_low_mr.mzML")
 	prun.AddProcess(sampleFilesSender)
 
-	peakPicker := sp.NewFromShell("PeakPicker", "PeakPickerHiRes -in {i:sample} -out {o:out} -ini "+workDir+"openms-params/PPparam.ini")
+	peakPicker := sp.NewFromShell("peakpicker", "PeakPickerHiRes -in {i:sample} -out {o:out} -ini "+workDir+"openms-params/PPparam.ini")
 	peakPicker.PathFormatters["out"] = func(t *sp.SciTask) string {
 		parts := str.Split(filepath.Base(t.GetInPath("sample")), ".")
 		outPath := workDir + "results/" + str.Join(parts[:len(parts)-1], "_") + ".peaks"
