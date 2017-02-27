fcc1c348158cc96a1f27eb01a7636280620d690c
Samuel Lampa <samuel.lampa@gmail.com>
2017-02-27 17:50:03 +0100

    Start fleshing out OpenMS workflow with first process

--- examples/k8s_openms/k8s_openms.go
+++ examples/k8s_openms/k8s_openms.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import sp "github.com/scipipe/scipipe"
+
+const (
+	workDir = "/scipipe-data/"
+)
 
 func main() {
-	fmt.Println("Open MS workflow soon to be implemented here!")
+	prun := sp.NewPipelineRunner()
+
+	peakPicker := sp.NewFromShell("PeakPicker", "PeakPickerHiRes -in {i:sample} -out {o:out} -ini {p:ini}")
+	peakPicker.PathFormatters["out"] = func(t *sp.SciTask) string {
+		//         filename = basename("{0}_{2}.{1}".format(*self.sampleFile.rsplit('.', 1) + ["peaks"]))
+		//         return luigi.LocalTarget("results/"+filename)
+		return "todo_implement_path_formatting.txt"
+	}
+
+	prun.AddProcess(peakPicker)
+
+	prun.Run()
 }
