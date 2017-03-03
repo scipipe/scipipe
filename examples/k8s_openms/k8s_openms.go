ac33424d00e19d9a76547428d2bc85a6b3d55c75
Samuel Lampa <samuel.lampa@gmail.com>
2017-03-03 13:54:08 +0100

    Enable processing multiple groupSuffixes in OpenMS example

--- examples/k8s_openms/k8s_openms.go
+++ examples/k8s_openms/k8s_openms.go
@@ -17,77 +17,81 @@ func main() {
 
 	prun := sp.NewPipelineRunner()
 
-	altNegLowMRFiles := spcomp.NewFileGlobber(workDir + "*alternate_neg_low_mr.mzML")
-	prun.AddProcess(altNegLowMRFiles)
-
-	// -------------------------------------------------------------------
-	// Peak Picker Process
-	// -------------------------------------------------------------------
-	peakPicker := sp.NewFromShell("peakpicker", "PeakPickerHiRes -in {i:sample} -out {o:peaks} -ini "+workDir+"openms-params/PPparam.ini")
-	peakPicker.PathFormatters["peaks"] = func(t *sp.SciTask) string {
-		parts := str.Split(filepath.Base(t.GetInPath("sample")), ".")
-		peaksPath := workDir + "results/" + str.Join(parts[:len(parts)-1], "_") + ".peaks"
-		return peaksPath
-	}
-	peakPicker.ExecMode = sp.ExecModeK8s
-	peakPicker.Image = "container-registry.phenomenal-h2020.eu/phnmnl/openms:v1.11.1_cv0.1.9"
-	prun.AddProcess(peakPicker)
-
-	// -------------------------------------------------------------------
-	// Feature Finder process
-	// -------------------------------------------------------------------
-	featFinder := sp.NewFromShell("featfinder", "FeatureFinderMetabo -in {i:peaks} -out {o:feats} -ini "+workDir+"openms-params/FFparam.ini")
-	featFinder.PathFormatters["feats"] = func(t *sp.SciTask) string {
-		featsPath := t.GetInPath("peaks") + ".featureXML"
-		return featsPath
-	}
-	featFinder.ExecMode = sp.ExecModeK8s
-	featFinder.Image = "container-registry.phenomenal-h2020.eu/phnmnl/openms:v1.11.1_cv0.1.9"
-	prun.AddProcess(featFinder)
-
-	// -------------------------------------------------------------------
-	// Feature Linker process
-	// -------------------------------------------------------------------
-	strToSubstr := spcomp.NewStreamToSubStream()
-	prun.AddProcess(strToSubstr)
-
-	featLinker := sp.NewFromShell("featlinker", "FeatureLinkerUnlabeledQT -in {i:feats:r: } -out {o:consensus} -ini "+workDir+"openms-params/FLparam.ini -threads 2")
-	featLinker.SetPathStatic("consensus", workDir+"results/"+"linked.consensusXML")
-	featLinker.ExecMode = sp.ExecModeK8s
-	featLinker.Image = "container-registry.phenomenal-h2020.eu/phnmnl/openms:v1.11.1_cv0.1.9"
-	prun.AddProcess(featLinker)
-
-	// -------------------------------------------------------------------
-	// File Filter process
-	// -------------------------------------------------------------------
-	fileFilter := sp.NewFromShell("filefilter", "FileFilter -in {i:unfiltered} -out {o:filtered} -ini "+workDir+"openms-params/FileFparam.ini")
-	fileFilter.SetPathReplace("unfiltered", "filtered", "linked", "linked_filtered")
-	fileFilter.ExecMode = sp.ExecModeK8s
-	fileFilter.Image = "container-registry.phenomenal-h2020.eu/phnmnl/openms:v1.11.1_cv0.1.9"
-	prun.AddProcess(fileFilter)
-
-	// -------------------------------------------------------------------
-	// Text Exporter process
-	// -------------------------------------------------------------------
-	textExporter := sp.NewFromShell("textexport", "TextExporter -in {i:consensus} -out {o:csv} -ini "+workDir+"openms-params/TEparam.ini")
-	textExporter.SetPathExtend("consensus", "csv", ".csv")
-	textExporter.ExecMode = sp.ExecModeK8s
-	textExporter.Image = "container-registry.phenomenal-h2020.eu/phnmnl/openms:v1.11.1_cv0.1.9"
-	prun.AddProcess(textExporter)
-
 	sink := sp.NewSink()
-	prun.AddProcess(sink)
+	groupSuffixes := []string{"alternate_neg_low_mr", "alternate_neg_high_mr"}
 
-	// -------------------------------------------------------------------
-	// Connect network
-	// -------------------------------------------------------------------
-	peakPicker.GetInPort("sample").Connect(altNegLowMRFiles.Out)
-	featFinder.GetInPort("peaks").Connect(peakPicker.GetOutPort("peaks"))
-	strToSubstr.In.Connect(featFinder.GetOutPort("feats"))
-	featLinker.GetInPort("feats").Connect(strToSubstr.OutSubStream)
-	fileFilter.GetInPort("unfiltered").Connect(featLinker.GetOutPort("consensus"))
-	textExporter.GetInPort("consensus").Connect(fileFilter.GetOutPort("filtered"))
-	sink.Connect(textExporter.GetOutPort("csv"))
+	for _, groupSuffix := range groupSuffixes {
+
+		altNegLowMRFiles := spcomp.NewFileGlobber(workDir + "*" + groupSuffix + ".mzML")
+		prun.AddProcess(altNegLowMRFiles)
+
+		// -------------------------------------------------------------------
+		// Peak Picker Process
+		// -------------------------------------------------------------------
+		peakPicker := sp.NewFromShell("peakpicker-"+str.Replace(groupSuffix, "_", "-", -1), "PeakPickerHiRes -in {i:sample} -out {o:peaks} -ini "+workDir+"openms-params/PPparam.ini")
+		peakPicker.PathFormatters["peaks"] = func(t *sp.SciTask) string {
+			parts := str.Split(filepath.Base(t.GetInPath("sample")), ".")
+			peaksPath := workDir + "results/" + str.Join(parts[:len(parts)-1], "_") + ".peaks"
+			return peaksPath
+		}
+		peakPicker.ExecMode = sp.ExecModeK8s
+		peakPicker.Image = "container-registry.phenomenal-h2020.eu/phnmnl/openms:v1.11.1_cv0.1.9"
+		prun.AddProcess(peakPicker)
+
+		// -------------------------------------------------------------------
+		// Feature Finder process
+		// -------------------------------------------------------------------
+		featFinder := sp.NewFromShell("featfinder-"+str.Replace(groupSuffix, "_", "-", -1), "FeatureFinderMetabo -in {i:peaks} -out {o:feats} -ini "+workDir+"openms-params/FFparam.ini")
+		featFinder.PathFormatters["feats"] = func(t *sp.SciTask) string {
+			featsPath := t.GetInPath("peaks") + ".featureXML"
+			return featsPath
+		}
+		featFinder.ExecMode = sp.ExecModeK8s
+		featFinder.Image = "container-registry.phenomenal-h2020.eu/phnmnl/openms:v1.11.1_cv0.1.9"
+		prun.AddProcess(featFinder)
+
+		// -------------------------------------------------------------------
+		// Feature Linker process
+		// -------------------------------------------------------------------
+		strToSubstr := spcomp.NewStreamToSubStream()
+		prun.AddProcess(strToSubstr)
+
+		featLinker := sp.NewFromShell("featlinker-"+str.Replace(groupSuffix, "_", "-", -1), "FeatureLinkerUnlabeledQT -in {i:feats:r: } -out {o:consensus} -ini "+workDir+"openms-params/FLparam.ini -threads 2")
+		featLinker.SetPathStatic("consensus", workDir+"results/linked_"+groupSuffix+".consensusXML")
+		featLinker.ExecMode = sp.ExecModeK8s
+		featLinker.Image = "container-registry.phenomenal-h2020.eu/phnmnl/openms:v1.11.1_cv0.1.9"
+		prun.AddProcess(featLinker)
+
+		// -------------------------------------------------------------------
+		// File Filter process
+		// -------------------------------------------------------------------
+		fileFilter := sp.NewFromShell("filefilter-"+str.Replace(groupSuffix, "_", "-", -1), "FileFilter -in {i:unfiltered} -out {o:filtered} -ini "+workDir+"openms-params/FileFparam.ini")
+		fileFilter.SetPathReplace("unfiltered", "filtered", "linked", "linked_filtered")
+		fileFilter.ExecMode = sp.ExecModeK8s
+		fileFilter.Image = "container-registry.phenomenal-h2020.eu/phnmnl/openms:v1.11.1_cv0.1.9"
+		prun.AddProcess(fileFilter)
+
+		// -------------------------------------------------------------------
+		// Text Exporter process
+		// -------------------------------------------------------------------
+		textExporter := sp.NewFromShell("textexport-"+str.Replace(groupSuffix, "_", "-", -1), "TextExporter -in {i:consensus} -out {o:csv} -ini "+workDir+"openms-params/TEparam.ini")
+		textExporter.SetPathExtend("consensus", "csv", ".csv")
+		textExporter.ExecMode = sp.ExecModeK8s
+		textExporter.Image = "container-registry.phenomenal-h2020.eu/phnmnl/openms:v1.11.1_cv0.1.9"
+		prun.AddProcess(textExporter)
+
+		// -------------------------------------------------------------------
+		// Connect network
+		// -------------------------------------------------------------------
+		peakPicker.GetInPort("sample").Connect(altNegLowMRFiles.Out)
+		featFinder.GetInPort("peaks").Connect(peakPicker.GetOutPort("peaks"))
+		strToSubstr.In.Connect(featFinder.GetOutPort("feats"))
+		featLinker.GetInPort("feats").Connect(strToSubstr.OutSubStream)
+		fileFilter.GetInPort("unfiltered").Connect(featLinker.GetOutPort("consensus"))
+		textExporter.GetInPort("consensus").Connect(fileFilter.GetOutPort("filtered"))
+		sink.Connect(textExporter.GetOutPort("csv"))
+	}
+	prun.AddProcess(sink)
 
 	prun.Run()
 }
