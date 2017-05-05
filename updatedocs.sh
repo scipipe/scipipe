b09daefd387618317b862488067ef222ac069c69
Samuel Lampa <samuel.lampa@gmail.com>
2017-05-05 13:36:41 +0200

    Add updatedocs.sh script

---
+++ updatedocs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+read -p "Please write a commit message for the doc change, enclosed by \"\":" msg
+echo "Committing changes in main repo ..."
+git commit -am "$msg"
+echo "Building docs site..."
+mkdocs build
+echo "Moving into site folder..."
+cd site/
+echo "Recreating CNAME file..."
+echo "scipipe.org" > CNAME
+echo "Adding new files to git ..."
+git add *
+echo "Committing changes in site repo..."
+git commit -am "$msg"
+echo "Pushing to docs site"
+git push
+echo "Moving back to main folder..."
+cd ../
+echo "Done."
