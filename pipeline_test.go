30a418f1ad64932dbd19f60bae83319e987ec5c8
Samuel Lampa <samuel.lampa@gmail.com>
2017-05-10 17:35:05 +0200

    Remove wrong stuff

--- pipeline_test.go
+++ pipeline_test.go
@@ -20,47 +20,6 @@ func TestAddProcesses(t *t.T) {
 	assert.IsType(t, &BogusProcess{}, pipeline.processes[1], "Process 2 was not of the right type!")
 }
 
-// ------------------------------------------------------------------------
-// This test fails when the race-detector is used, as go-routines which are
-// otherwise idle, are then run, as it seems.  If this is an expected behavior
-// of the race detector, this test is not correct anyway. Investigating.
-// ------------------------------------------------------------------------
-// func TestRunProcessesInPipelineRunner(t *t.T) {
-// 	proc1 := NewBogusProcess()
-// 	proc2 := NewBogusProcess()
-//
-// 	pipeline := NewPipelineRunner()
-// 	pipeline.AddProcesses(proc1, proc2)
-// 	pipeline.Run()
-//
-// 	// Only the last process is supposed to be run by the pipeline directly,
-// 	// while the others are only run if an output is pulled on an out-port,
-// 	// but since we haven't connected the tasks here, only the last one
-// 	// should be ran in this case.
-// 	proc1.WasRunLock.Lock()
-// 	assert.False(t, proc1.WasRun, "Process 1 was run!")
-// 	proc1.WasRunLock.Unlock()
-//
-// 	proc2.WasRunLock.Lock()
-// 	assert.True(t, proc2.WasRun, "Process 2 was not run!")
-// 	proc2.WasRunLock.Unlock()
-// }
-// ------------------------------------------------------------------------
-
-func ExamplePrintProcesses() {
-	proc1 := NewBogusProcess()
-	proc2 := NewBogusProcess()
-
-	pipeline := NewPipelineRunner()
-	pipeline.AddProcesses(proc1, proc2)
-	pipeline.Run()
-
-	pipeline.PrintProcesses()
-	// Output:
-	// Process 0: *scipipe.BogusProcess
-	// Process 1: *scipipe.BogusProcess
-}
-
 // --------------------------------
 // Helper stuff
 // --------------------------------
