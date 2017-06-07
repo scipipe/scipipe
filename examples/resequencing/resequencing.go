57553e9b03f740c39b1495e56e61891fdc60ead1
Samuel Lampa <samuel.lampa@gmail.com>
2017-06-07 22:28:56 +0200

    Put back separators and clean up package comment in resequencing example

--- examples/resequencing/resequencing.go
+++ examples/resequencing/resequencing.go
@@ -2,14 +2,14 @@
 // to teach the introductory NGS bioinformatics analysis course at SciLifeLab
 // as described on this page:
 // http://uppnex.se/twiki/do/view/Courses/NgsIntro1502/ResequencingAnalysis.html
-// Prerequisites:
-// - Samtools
-// - BWA
-// - Picard
-// - GATK
-// Install all tools except GATK like this on X/L/K/Ubuntu:
-// sudo apt-get install samtools bwa picard-tools
-// (GATK needs to be downloaded and installed manually from www.broadinstitute.org/gatk)
+//
+// Prerequisites: Samtools, BWA, Picard, GATK.  You can install all tools
+// except GATK on X/L/K/Ubuntu, with this command:
+//
+//   sudo apt-get install samtools bwa picard-tools
+//
+// GATK needs to be downloaded and installed manually from:
+// http://www.broadinstitute.org/gatk
 package main
 
 import (
@@ -18,6 +18,9 @@ import (
 	. "github.com/scipipe/scipipe"
 )
 
+// ------------------------------------------------------------------------------------
+// Set up static stuff like paths
+// ------------------------------------------------------------------------------------
 const (
 	fastq_base_url = "http://bioinfo.perdanauniversity.edu.my/tein4ngs/ngspractice/"
 	fastq_file_pat = "%s.ILLUMINA.low_coverage.4p_%s.fq"
@@ -28,27 +31,38 @@ const (
 	vcf_file       = "ALL.chr17.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.vcf.gz"
 )
 
+// ------------------------------------------------------------------------------------
+// Set up the main input parameters to the workflow
+// ------------------------------------------------------------------------------------
 var (
 	individuals = []string{"NA06984", "NA12489"}
 	samples     = []string{"1", "2"}
 )
 
 func main() {
-	// Initialize workflow
+	// --------------------------------------------------------------------------------
+	// Initialize Workflow
+	// --------------------------------------------------------------------------------
 	wf := NewWorkflow("resequencing_wf")
 
+	// --------------------------------------------------------------------------------
 	// Download Reference Genome
+	// --------------------------------------------------------------------------------
 	downloadRefCmd := "wget -O {o:outfile} " + ref_base_url + ref_file_gz
 	downloadRef := wf.NewProc("download_ref", downloadRefCmd)
 	downloadRef.SetPathStatic("outfile", ref_file_gz)
 
+	// --------------------------------------------------------------------------------
 	// Unzip ref file
+	// --------------------------------------------------------------------------------
 	ungzipRefCmd := "gunzip -c {i:in} > {o:out}"
 	ungzipRef := wf.NewProc("ugzip_ref", ungzipRefCmd)
 	ungzipRef.SetPathReplace("in", "out", ".gz", "")
 	ungzipRef.In("in").Connect(downloadRef.Out("outfile"))
 
+	// --------------------------------------------------------------------------------
 	// Index Reference Genome
+	// --------------------------------------------------------------------------------
 	indexRef := wf.NewProc("index_ref", "bwa index -a bwtsw {i:index}; echo done > {o:done}")
 	indexRef.SetPathExtend("index", "done", ".indexed")
 	indexRef.In("index").Connect(ungzipRef.Out("out"))
@@ -62,7 +76,9 @@ func main() {
 			outPorts[indv][smpl] = map[string]*FilePort{}
 			indv_smpl := "_" + indv + "_" + smpl
 
+			// ------------------------------------------------------------------------
 			// Download FastQ component
+			// ------------------------------------------------------------------------
 			file_name := fmt.Sprintf(fastq_file_pat, indv, smpl)
 			downloadFastQCmd := "wget -O {o:fastq} " + fastq_base_url + file_name
 			downloadFastQ := wf.NewProc("download_fastq"+indv_smpl, downloadFastQCmd)
@@ -71,7 +87,9 @@ func main() {
 			// Save outPorts for later use
 			outPorts[indv][smpl]["fastq"] = downloadFastQ.Out("fastq")
 
+			// ------------------------------------------------------------------------
 			// BWA Align
+			// ------------------------------------------------------------------------
 			bwaAlignCmd := "bwa aln {i:ref} {i:fastq} > {o:sai} # {i:idxdone}"
 			bwaAlign := wf.NewProc("bwa_aln"+indv_smpl, bwaAlignCmd)
 			bwaAlign.SetPathExtend("fastq", "sai", ".sai")
@@ -83,7 +101,9 @@ func main() {
 			outPorts[indv][smpl]["sai"] = bwaAlign.Out("sai")
 		}
 
+		// ---------------------------------------------------------------------------
 		// Merge
+		// ---------------------------------------------------------------------------
 		bwaMergeCmd := "bwa sampe {i:ref} {i:sai1} {i:sai2} {i:fq1} {i:fq2} > {o:merged} # {i:refdone}"
 		bwaMerge := wf.NewProc("merge_"+indv, bwaMergeCmd)
 		bwaMerge.SetPathCustom("merged", func(t *SciTask) string {
@@ -100,6 +120,8 @@ func main() {
 		wf.ConnectLast(bwaMerge.Out("merged"))
 	}
 
-	// Run pipeline
+	// -------------------------------------------------------------------------------
+	// Run Workflow
+	// -------------------------------------------------------------------------------
 	wf.Run()
 }
