8cd76e42202272f4d643a2e858023a889ed01205
Samuel Lampa <samuel.lampa@gmail.com>
2017-06-07 22:20:21 +0200

    Simplifying resequecing workflow after multisend-ports

--- examples/resequencing/resequencing.go
+++ examples/resequencing/resequencing.go
@@ -16,7 +16,6 @@ import (
 	"fmt"
 
 	. "github.com/scipipe/scipipe"
-	comp "github.com/scipipe/scipipe/components"
 )
 
 const (
@@ -35,44 +34,24 @@ var (
 )
 
 func main() {
-
-	// --------------------------------------------------------------------------------
-	// Initialize pipeline runner
-	// --------------------------------------------------------------------------------
-
+	// Initialize workflow
 	wf := NewWorkflow("resequencing_wf")
 
-	// --------------------------------------------------------------------------------
 	// Download Reference Genome
-	// --------------------------------------------------------------------------------
 	downloadRefCmd := "wget -O {o:outfile} " + ref_base_url + ref_file_gz
 	downloadRef := wf.NewProc("download_ref", downloadRefCmd)
 	downloadRef.SetPathStatic("outfile", ref_file_gz)
 
-	// --------------------------------------------------------------------------------
 	// Unzip ref file
-	// --------------------------------------------------------------------------------
 	ungzipRefCmd := "gunzip -c {i:in} > {o:out}"
 	ungzipRef := wf.NewProc("ugzip_ref", ungzipRefCmd)
 	ungzipRef.SetPathReplace("in", "out", ".gz", "")
 	ungzipRef.In("in").Connect(downloadRef.Out("outfile"))
 
-	// Create a FanOut so multiple downstream processes can read from the
-	// ungzip process
-	refFanOut := comp.NewFanOut("ref_fanout")
-	refFanOut.InFile.Connect(ungzipRef.Out("out"))
-	wf.Add(refFanOut)
-
-	// --------------------------------------------------------------------------------
 	// Index Reference Genome
-	// --------------------------------------------------------------------------------
 	indexRef := wf.NewProc("index_ref", "bwa index -a bwtsw {i:index}; echo done > {o:done}")
 	indexRef.SetPathExtend("index", "done", ".indexed")
-	indexRef.In("index").Connect(refFanOut.Out("index_ref"))
-
-	indexDoneFanOut := comp.NewFanOut("indexdone_fanout")
-	indexDoneFanOut.InFile.Connect(indexRef.Out("done"))
-	wf.Add(indexDoneFanOut)
+	indexRef.In("index").Connect(ungzipRef.Out("out"))
 
 	// Create (multi-level) maps where we can gather outports from processes
 	// for each for loop iteration and access them in the merge step later
@@ -82,50 +61,37 @@ func main() {
 		for _, smpl := range samples {
 			outPorts[indv][smpl] = map[string]*FilePort{}
 			indv_smpl := "_" + indv + "_" + smpl
-			// --------------------------------------------------------------------------------
+
 			// Download FastQ component
-			// --------------------------------------------------------------------------------
 			file_name := fmt.Sprintf(fastq_file_pat, indv, smpl)
 			downloadFastQCmd := "wget -O {o:fastq} " + fastq_base_url + file_name
 			downloadFastQ := wf.NewProc("download_fastq"+indv_smpl, downloadFastQCmd)
 			downloadFastQ.SetPathStatic("fastq", file_name)
 
-			fastQFanOut := comp.NewFanOut("fastq_fanout" + indv_smpl)
-			fastQFanOut.InFile.Connect(downloadFastQ.Out("fastq"))
-			wf.Add(fastQFanOut)
-
 			// Save outPorts for later use
-			outPorts[indv][smpl]["fastq"] = fastQFanOut.Out("merg")
+			outPorts[indv][smpl]["fastq"] = downloadFastQ.Out("fastq")
 
-			// --------------------------------------------------------------------------------
 			// BWA Align
-			// --------------------------------------------------------------------------------
 			bwaAlignCmd := "bwa aln {i:ref} {i:fastq} > {o:sai} # {i:idxdone}"
 			bwaAlign := wf.NewProc("bwa_aln"+indv_smpl, bwaAlignCmd)
 			bwaAlign.SetPathExtend("fastq", "sai", ".sai")
-			bwaAlign.In("ref").Connect(refFanOut.Out("bwa_aln_" + indv + "_" + smpl))
-			bwaAlign.In("idxdone").Connect(indexDoneFanOut.Out("bwa_aln_" + indv + "_" + smpl))
-			bwaAlign.In("fastq").Connect(fastQFanOut.Out("bwa_aln"))
+			bwaAlign.In("ref").Connect(ungzipRef.Out("out"))
+			bwaAlign.In("idxdone").Connect(indexRef.Out("done"))
+			bwaAlign.In("fastq").Connect(downloadFastQ.Out("fastq"))
 
 			// Save outPorts for later use
 			outPorts[indv][smpl]["sai"] = bwaAlign.Out("sai")
 		}
 
-		// --------------------------------------------------------------------------------
 		// Merge
-		// --------------------------------------------------------------------------------
-		// This one is is needed so bwaMergecan take a proper parameter for
-		// individual, which it uses to generate output paths
-
-		// bwa sampe process
 		bwaMergeCmd := "bwa sampe {i:ref} {i:sai1} {i:sai2} {i:fq1} {i:fq2} > {o:merged} # {i:refdone}"
 		bwaMerge := wf.NewProc("merge_"+indv, bwaMergeCmd)
 		bwaMerge.SetPathCustom("merged", func(t *SciTask) string {
 			indv := indv
 			return fmt.Sprintf("%s.merged.sam", indv)
 		})
-		bwaMerge.In("ref").Connect(refFanOut.Out("bwa_merge_" + indv))
-		bwaMerge.In("refdone").Connect(indexDoneFanOut.Out("bwa_merge_" + indv))
+		bwaMerge.In("ref").Connect(ungzipRef.Out("out"))
+		bwaMerge.In("refdone").Connect(indexRef.Out("done"))
 		bwaMerge.In("sai1").Connect(outPorts[indv]["1"]["sai"])
 		bwaMerge.In("sai2").Connect(outPorts[indv]["2"]["sai"])
 		bwaMerge.In("fq1").Connect(outPorts[indv]["1"]["fastq"])
@@ -134,9 +100,6 @@ func main() {
 		wf.ConnectLast(bwaMerge.Out("merged"))
 	}
 
-	// --------------------------------------------------------------------------------
 	// Run pipeline
-	// --------------------------------------------------------------------------------
-
 	wf.Run()
 }
