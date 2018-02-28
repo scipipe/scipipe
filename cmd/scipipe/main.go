a00de3c1c1743dcd2bea312354a9e68755d15e2b
Samuel Lampa <samuel.lampa@gmail.com>
2018-03-01 00:30:01 +0100

    Remove dependency: github.com/urfave/cli

--- cmd/scipipe/main.go
+++ cmd/scipipe/main.go
@@ -1,26 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	"github.com/scipipe/scipipe"
-
-	"github.com/urfave/cli"
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "The SciPipe Tool"
-	app.Usage = "A helper tool to ease working with SciPipe workflows"
-	app.Version = "0.0.1"
-	app.Commands = []cli.Command{
-		{
-			Name:    "new",
-			Aliases: []string{"n"},
-			Usage:   "Create a new example workflow file, in a filename provided as first argument after 'new' (default: mynewworkflow.go).",
-			Action: func(c *cli.Context) error {
-				wfcode := `// Workflow written in SciPipe.
+	flag.Parse()
+	cmd := flag.Arg(0)
+	if cmd == "new" {
+		wfcode := `// Workflow written in SciPipe.
 // For more information about SciPipe, see: http://scipipe.org
 package main
 
@@ -43,27 +35,30 @@ func main() {
 	// Run the workflow
 	wf.Run()
 }`
-				fileName := c.Args().First()
-				if fileName == "" {
-					fileName = "my_workflow.go"
-					fmt.Printf("No filename specified, so using the default '%s' ...\n", fileName)
-				}
+		fileName := flag.Arg(1)
+		if fileName == "" {
+			fmt.Println("ERROR: No filename specified!\n")
+			printUsage()
+			os.Exit(1)
+		}
 
-				f, err := os.Create(fileName)
-				if err != nil {
-					scipipe.Fail("Could not create file:", fileName)
-				}
-				defer f.Close()
+		f, err := os.Create(fileName)
+		if err != nil {
+			scipipe.Fail("Could not create file:", fileName)
+		}
+		defer f.Close()
 
-				_, err = f.Write([]byte(wfcode))
-				if err != nil {
-					scipipe.Fail("Could not write to file:", fileName)
-				}
+		_, err = f.Write([]byte(wfcode))
+		if err != nil {
+			scipipe.Fail("Could not write to file:", fileName)
+		}
 
-				fmt.Println("Successfully wrote new workflow file to:", fileName)
-				return nil
-			},
-		},
+		fmt.Println("Successfully wrote new workflow file to:", fileName, "\n\nNow you can run it with:\ngo run ", fileName)
+	} else {
+		printUsage()
 	}
-	app.Run(os.Args)
+}
+
+func printUsage() {
+	fmt.Println("Usage of scipipe:\nscipipe new <filename.go>")
 }
