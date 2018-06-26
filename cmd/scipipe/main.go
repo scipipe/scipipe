b562158836f6c6a7825dfd9eb2099717f61a95dd
Samuel Lampa <samuel.lampa@gmail.com>
2018-06-26 21:22:17 +0200

    Add audit2html subcommand in scipipe command

--- cmd/scipipe/main.go
+++ cmd/scipipe/main.go
@@ -4,23 +4,75 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
+	"github.com/pkg/errors"
 	"github.com/scipipe/scipipe"
 )
 
 func main() {
+	scipipe.InitLogError()
+
 	flag.Parse()
 	cmd := flag.Arg(0)
 	switch cmd {
 	case "new":
+		if len(flag.Args()) < 2 {
+			fmt.Println("ERROR: No filename specified!")
+			printNewUsage()
+			os.Exit(1)
+		}
 		writeNewWorkflowFile(flag.Arg(1))
+	case "audit2html":
+		if len(flag.Args()) < 2 {
+			fmt.Println("ERROR: No infile specified!")
+			printAudit2HTMLUsage()
+			os.Exit(1)
+		}
+		inFile := flag.Arg(1)
+		if len(flag.Args()) < 3 {
+			fmt.Println("ERROR: No outfile specified!")
+			printAudit2HTMLUsage()
+			os.Exit(1)
+		}
+		outFile := flag.Arg(2)
+		err := convertAudit2Html(inFile, outFile)
+		if err != nil {
+			scipipe.CheckWithMsg(err, "Could not convert Audit file to HTML")
+		}
 	default:
-		printUsage()
+		printHelp()
 	}
 }
 
-func printUsage() {
-	fmt.Println("Usage of scipipe:\nscipipe new <filename.go>")
+func printNewUsage() {
+	fmt.Print(`
+Usage:
+$ scipipe new <filename.go>
+`)
+}
+func printAudit2HTMLUsage() {
+	fmt.Print(`
+Usage:
+$ scipipe audit2html <infile.audit.json> [<outfile.html>]
+`)
+}
+
+func printHelp() {
+	fmt.Printf(`________________________________________________________________________
+
+SciPipe (http://scipipe.org)
+Version: %s
+________________________________________________________________________
+
+Usage:
+$ scipipe <command> [command options]
+
+Available commands:
+$ scipipe new <filename.go>
+$ scipipe audit2html <infile.audit.json> [<outfile.html>]
+________________________________________________________________________
+`, scipipe.Version)
 }
 
 func writeNewWorkflowFile(fileName string) {
@@ -47,11 +99,6 @@ func main() {
 	// Run the workflow
 	wf.Run()
 }`
-	if fileName == "" {
-		fmt.Println("ERROR: No filename specified!")
-		printUsage()
-		os.Exit(1)
-	}
 	f, err := os.Create(fileName)
 	if err != nil {
 		scipipe.Fail("Could not create file:", fileName)
@@ -62,4 +109,56 @@ func main() {
 		scipipe.Fail("Could not write to file:", fileName)
 	}
 	fmt.Println("Successfully wrote new workflow file to:", fileName, "\n\nNow you can run it with:\ngo run ", fileName)
+}
+
+func convertAudit2Html(inFilePath string, outFilePath string) error {
+	ip := scipipe.NewFileIP(strings.Replace(inFilePath, ".audit.json", "", 1))
+	ai := ip.AuditInfo()
+
+	outHTML := fmt.Sprintf(`<html><head><style>body { font-family: arial, helvetica, sans-serif; } table { border: 1px solid #ccc; } th { text-align: right; vertical-align: top; padding: .2em .8em; } td { vertical-align: top; }</style><title>Audit info for: %s</title></head><body><h2>Audit Info for: %s</h2>`, ip.Path(), ip.Path())
+	outHTML += formatAuditInfoHTML(ai)
+	outHTML += `</body></html>`
+
+	if _, err := os.Stat(outFilePath); os.IsExist(err) {
+		return errors.Wrap(err, "File already exists:"+outFilePath)
+	}
+	outFile, err := os.Create(outFilePath)
+	if err != nil {
+		return errors.Wrap(err, "Could not create file:"+outFilePath)
+	}
+	outFile.WriteString(outHTML)
+	outFile.Close()
+	return nil
+}
+
+func formatAuditInfoHTML(ai *scipipe.AuditInfo) (outHTML string) {
+	outHTML = "<table>\n"
+	outHTML += fmt.Sprintf("<tr><th>ID:</th><td>%s</td></tr>\n", ai.ID)
+	outHTML += fmt.Sprintf("<tr><th>Process name:</th><td>%s</td></tr>\n", ai.ProcessName)
+	outHTML += fmt.Sprintf("<tr><th>Command:</th><td><pre>%s</pre></td></tr>\n", ai.Command)
+
+	params := []string{}
+	for pname, p := range ai.Params {
+		params = append(params, fmt.Sprintf("%s: %s", pname, p))
+	}
+	outHTML += fmt.Sprintf("<tr><th>Parameters:</th><td>%s</td></tr>\n", strings.Join(params, ", "))
+	tags := []string{}
+	for pname, p := range ai.Tags {
+		tags = append(tags, fmt.Sprintf("%s: %s", pname, p))
+	}
+	outHTML += fmt.Sprintf("<tr><th>Tags:</th><td><pre>%v</pre></td></tr>\n", strings.Join(tags, ", "))
+
+	outHTML += fmt.Sprintf("<tr><th>Start time:</th><td>%v</td></tr>\n", ai.StartTime)
+	outHTML += fmt.Sprintf("<tr><th>Finish time:</th><td>%v</td></tr>\n", ai.FinishTime)
+	outHTML += fmt.Sprintf("<tr><th>Execution time:</th><td>%d ms</td></tr>\n", ai.ExecTimeMS)
+	upStreamHTML := ""
+	for filePath, uai := range ai.Upstream {
+		upStreamHTML += `<h4> Audit info for: ` + filePath + `</h4>`
+		upStreamHTML += formatAuditInfoHTML(uai)
+	}
+	if outHTML != "" {
+		outHTML += "<tr><th>Upstreams:</th><td>" + upStreamHTML + "</td></tr>\n"
+	}
+	outHTML += "</table>\n"
+	return
 }
