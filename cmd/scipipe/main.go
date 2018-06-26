829d05f0e186aad02000c394ab6799eafe3f2076
Samuel Lampa <samuel.lampa@gmail.com>
2018-06-26 20:59:42 +0200

    Refactor scipipe command

--- cmd/scipipe/main.go
+++ cmd/scipipe/main.go
@@ -11,8 +11,20 @@ import (
 func main() {
 	flag.Parse()
 	cmd := flag.Arg(0)
-	if cmd == "new" {
-		wfcode := `// Workflow written in SciPipe.
+	switch cmd {
+	case "new":
+		writeNewWorkflowFile(flag.Arg(1))
+	default:
+		printUsage()
+	}
+}
+
+func printUsage() {
+	fmt.Println("Usage of scipipe:\nscipipe new <filename.go>")
+}
+
+func writeNewWorkflowFile(fileName string) {
+	wfcode := `// Workflow written in SciPipe.
 // For more information about SciPipe, see: http://scipipe.org
 package main
 
@@ -35,30 +47,19 @@ func main() {
 	// Run the workflow
 	wf.Run()
 }`
-		fileName := flag.Arg(1)
-		if fileName == "" {
-			fmt.Println("ERROR: No filename specified!")
-			printUsage()
-			os.Exit(1)
-		}
-
-		f, err := os.Create(fileName)
-		if err != nil {
-			scipipe.Fail("Could not create file:", fileName)
-		}
-		defer f.Close()
-
-		_, err = f.Write([]byte(wfcode))
-		if err != nil {
-			scipipe.Fail("Could not write to file:", fileName)
-		}
-
-		fmt.Println("Successfully wrote new workflow file to:", fileName, "\n\nNow you can run it with:\ngo run ", fileName)
-	} else {
+	if fileName == "" {
+		fmt.Println("ERROR: No filename specified!")
 		printUsage()
+		os.Exit(1)
 	}
-}
-
-func printUsage() {
-	fmt.Println("Usage of scipipe:\nscipipe new <filename.go>")
+	f, err := os.Create(fileName)
+	if err != nil {
+		scipipe.Fail("Could not create file:", fileName)
+	}
+	defer f.Close()
+	_, err = f.Write([]byte(wfcode))
+	if err != nil {
+		scipipe.Fail("Could not write to file:", fileName)
+	}
+	fmt.Println("Successfully wrote new workflow file to:", fileName, "\n\nNow you can run it with:\ngo run ", fileName)
 }
