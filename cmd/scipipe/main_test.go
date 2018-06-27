a79836fb890927a7d83234768e9b1dc80d64e934
Samuel Lampa <samuel.lampa@gmail.com>
2018-06-27 16:50:25 +0200

    Extend test of scipipe command

--- cmd/scipipe/main_test.go
+++ cmd/scipipe/main_test.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
 func TestNewCmd(t *testing.T) {
 	initLogsTest()
 
-	args := []string{"new", "/tmp/testwf.go"}
+	testWfPath := "/tmp/testwf.go"
+
+	args := []string{"new", testWfPath}
 	err := parseFlags(args)
 	if err != nil {
 		t.Error("Could not parse flags:", err.Error())
+	}
+
+	if _, err := os.Stat(testWfPath); os.IsNotExist(err) {
+		t.Error(t, "`scipipe new` command failed to create new workflow file: "+testWfPath)
+	}
+
+	cleanFiles(t, testWfPath)
+}
+
+func cleanFiles(t *testing.T, files ...string) {
+	for _, f := range files {
+		err := os.Remove(f)
+		if err != nil {
+			t.Error(err.Error())
+		}
 	}
 }
