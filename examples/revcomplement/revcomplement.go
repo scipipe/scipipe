44784477c956045ae93582d42580c0988f6fc044
Samuel Lampa <samuel.lampa@gmail.com>
2018-07-16 22:27:18 +0200

    Improve reverse complement example

--- examples/revcomplement/revcomplement.go
+++ examples/revcomplement/revcomplement.go
@@ -4,18 +4,24 @@ import (
 	"github.com/scipipe/scipipe"
 )
 
-const dna = "AAACCCGGGTTT"
+const dna = "AAAGCCCGTGGGGGACCTGTTC"
 
 func main() {
-	// Initialize workflow
+	// Initialize workflow, using max 4 CPU cores
 	wf := scipipe.NewWorkflow("DNA Base Complement Workflow", 4)
 
-	// Initialize processes based on shell commands
+	// Initialize processes based on shell commands:
+	// makeDNA writes a DNA string to a file
 	makeDNA := wf.NewProc("Make DNA", "echo "+dna+" > {o:dna}")
-	complmt := wf.NewProc("Base Complement", `cat {i:in} | tr ATCG TAGC > {o:compl}`)
+	makeDNA.SetOut("dna", "dna.txt")
+	// complmt computes the base complement of a DNA string
+	complmt := wf.NewProc("Base Complement", "cat {i:in} | tr ATCG TAGC > {o:compl}")
+	complmt.SetOut("compl", "{i:in}.compl.txt")
+	// reverse reverses the input DNA string
 	reverse := wf.NewProc("Reverse", "cat {i:in} | rev > {o:rev}")
+	reverse.SetOut("rev", "{i:in}.rev.txt")
 
-	// Connect dependencies
+	// Connect data dependencies between out- and in-ports
 	complmt.In("in").From(makeDNA.Out("dna"))
 	reverse.In("in").From(complmt.Out("compl"))
 
