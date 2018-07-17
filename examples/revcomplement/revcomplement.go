45f10c40c7852b2d5613effefdd0acfcc242b55b
Samuel Lampa <samuel.lampa@gmail.com>
2018-07-18 00:07:57 +0200

    Use new strip-from-end in revcomplement example

--- examples/revcomplement/revcomplement.go
+++ examples/revcomplement/revcomplement.go
@@ -11,15 +11,16 @@ func main() {
 	wf := scipipe.NewWorkflow("DNA Base Complement Workflow", 4)
 
 	// Initialize processes based on shell commands:
+
 	// makeDNA writes a DNA string to a file
 	makeDNA := wf.NewProc("Make DNA", "echo "+dna+" > {o:dna}")
 	makeDNA.SetOut("dna", "dna.txt")
 	// complmt computes the base complement of a DNA string
 	complmt := wf.NewProc("Base Complement", "cat {i:in} | tr ATCG TAGC > {o:compl}")
-	complmt.SetOut("compl", "{i:in}.compl.txt")
+	complmt.SetOut("compl", "{i:in|%.txt}.compl.txt")
 	// reverse reverses the input DNA string
 	reverse := wf.NewProc("Reverse", "cat {i:in} | rev > {o:rev}")
-	reverse.SetOut("rev", "{i:in}.rev.txt")
+	reverse.SetOut("rev", "{i:in|%.txt}.rev.txt")
 
 	// Connect data dependencies between out- and in-ports
 	complmt.In("in").From(makeDNA.Out("dna"))
