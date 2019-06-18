457fea6c634ff9d306bb0e3ce1524e7eee4bbb9a
Drayton Munster <drayton.w.munster@nasa.gov>
2019-06-18 12:13:02 -0400

    Fix tempdir paths for file_combinator_test

--- components/file_combinator_test.go
+++ components/file_combinator_test.go
@@ -2,7 +2,9 @@ package components
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"log"
@@ -15,9 +17,14 @@ var numbers = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", 
 
 func TestFileCombinator(t *testing.T) {
 
+	tmpDir, err := ioutil.TempDir("", "TestFileCombinator")
+	if err != nil {
+		log.Fatal("could not create tmpDir: ", err)
+	}
+
 	// Create letter files
 	for _, s := range letters {
-		fName := "/tmp/letterfile_" + s + ".txt"
+		fName := filepath.Join(tmpDir, "letterfile_"+s+".txt")
 		f, err := os.Create(fName)
 		if err != nil {
 			log.Fatalf("File could not be created: %s\n", fName)
@@ -27,7 +34,7 @@ func TestFileCombinator(t *testing.T) {
 
 	// Create number files
 	for _, s := range numbers {
-		fName := "/tmp/numberfile_" + s + ".txt"
+		fName := filepath.Join(tmpDir, "numberfile_"+s+".txt")
 		f, err := os.Create(fName)
 		if err != nil {
 			log.Fatalf("File could not be created: %s\n", fName)
@@ -38,8 +45,8 @@ func TestFileCombinator(t *testing.T) {
 	// Create workflow
 	wf := scipipe.NewWorkflow("wf", 4)
 
-	letterGlobber := NewFileGlobber(wf, "letter_globber", "/tmp/letterfile_*.txt")
-	numberGlobber := NewFileGlobber(wf, "number_globber", "/tmp/numberfile_*.txt")
+	letterGlobber := NewFileGlobber(wf, "letter_globber", filepath.Join(tmpDir, "letterfile_*.txt"))
+	numberGlobber := NewFileGlobber(wf, "number_globber", filepath.Join(tmpDir, "numberfile_*.txt"))
 
 	fileCombiner := NewFileCombinator(wf, "file_combiner")
 	fileCombiner.In("letters").From(letterGlobber.Out())
@@ -48,13 +55,13 @@ func TestFileCombinator(t *testing.T) {
 	catenator := wf.NewProc("catenator", "cat {i:letters} {i:numbers} > {o:combined}")
 	catenator.In("letters").From(fileCombiner.Out("letters"))
 	catenator.In("numbers").From(fileCombiner.Out("numbers"))
-	catenator.SetOut("combined", "/tmp/combined/{i:letters|basename|%.txt}.{i:numbers|basename|%.txt}.combined.txt")
+	catenator.SetOut("combined", filepath.Join(tmpDir, "combined", "{i:letters|basename|%.txt}.{i:numbers|basename|%.txt}.combined.txt"))
 
 	wf.Run()
 
 	for _, l := range letters {
 		for _, n := range numbers {
-			filePath := fmt.Sprintf("/tmp/combined/letterfile_%s.numberfile_%s.combined.txt", l, n)
+			filePath := fmt.Sprintf(filepath.Join(tmpDir, "combined", "letterfile_%s.numberfile_%s.combined.txt"), l, n)
 			if _, err := os.Stat(filePath); os.IsNotExist(err) {
 				log.Fatal("File did not exist: " + filePath)
 			}
@@ -64,17 +71,19 @@ func TestFileCombinator(t *testing.T) {
 	// Clean up files
 	filePaths := []string{}
 	for _, s := range letters {
-		filePaths = append(filePaths, fmt.Sprintf("/tmp/letterfile_%s.txt", s))
+		filePaths = append(filePaths, fmt.Sprintf(filepath.Join(tmpDir, "letterfile_%s.txt"), s))
 	}
 	for _, s := range numbers {
-		filePaths = append(filePaths, fmt.Sprintf("/tmp/numberfile_%s.txt", s))
+		filePaths = append(filePaths, fmt.Sprintf(filepath.Join(tmpDir, "numberfile_%s.txt"), s))
 	}
 	for _, l := range letters {
 		for _, n := range numbers {
-			filePaths = append(filePaths, fmt.Sprintf("/tmp/combined/letterfile_%s.numberfile_%s.combined.txt", l, n))
+			filePaths = append(filePaths, fmt.Sprintf(filepath.Join(tmpDir, "combined", "letterfile_%s.numberfile_%s.combined.txt"), l, n))
 			filePaths = append(filePaths, filePaths[len(filePaths)-1]+".audit.json")
 		}
 	}
+	filePaths = append(filePaths)
+
 	for _, filePath := range filePaths {
 		err := os.Remove(filePath)
 		if err != nil {
