3820a3019624b4ac5c0a256682812dc1df6f381a
Samuel Lampa <samuel.lampa@rilnet.com>
2020-08-26 11:32:28 +0200

    Bump version to 0.9.8

--- const.go
+++ const.go
@@ -4,5 +4,5 @@ const (
 	// BUFSIZE is the standard buffer size used for channels connecting processes
 	BUFSIZE = 128
 	// Version is the SciPipe version in string format
-	Version = "0.9.6"
+	Version = "0.9.8"
 )
