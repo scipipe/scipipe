766b9708f4d2991acb1744efbe93eac6f9fc28f2
Samuel Lampa <samuel.lampa@rilnet.com>
2021-03-08 23:16:16 +0100

    Fix updatedocs.sh

--- updatedocs.sh
+++ updatedocs.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-read -p "Please write a commit message for the doc change: " msg
-echo "Committing changes in main repo ..."
-git commit -am "$msg"
-echo "Building docs site..."
-mkdocs build
-echo "Moving into site folder..."
-cd site/
-echo "Recreating CNAME file..."
-echo "scipipe.org" > CNAME
-echo "Adding new files to git ..."
-git add *
-echo "Committing changes in site repo..."
-git commit -am "$msg"
-echo "Pushing to docs site"
-git push
-echo "Moving back to main folder..."
-cd ../
+if [[ ! -d site ]]; then
+    git clone git@github.com:scipipe/scipipe.github.io.git site
+fi && \
+echo "Building docs site..." && \
+mkdocs build && \
+echo "Moving into site folder..." && \
+cd site/ && \
+echo "Recreating CNAME file..." && \
+echo "scipipe.org" > CNAME && \
+echo "Adding new files to git ..." && \
+git add * && \
+echo "Committing changes in site repo..." && \
+git commit -am "Update docs" && \
+echo "Pushing to docs site" && \
+git push && \
+echo "Moving back to main folder..." && \
+cd ../ && \
 echo "Done."
