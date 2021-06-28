78ede95bbbfab88d04ba51e9de1f753bf222073c
Samuel Lampa <samuel.lampa@rilnet.com>
2021-06-29 00:09:53 +0200

    Improve test of IPSelectorSync component

--- components/ip_selector_sync_test.go
+++ components/ip_selector_sync_test.go
@@ -11,12 +11,20 @@ func TestIPSelectorSync(t *testing.T) {
 	wf := sp.NewWorkflow("testwf", 4)
 
 	// Create a non-empty file
-	p1 := wf.NewProc("p1", "echo foo > {o:foo}")
-	p1.SetOut("foo", ".tmp/foo.txt")
+	p1a := wf.NewProc("p1a", "echo foo > {o:foo}")
+	p1a.SetOut("foo", ".tmp/foo.txt")
 
 	// Create an EMPTY file
-	p2 := wf.NewProc("p2", "touch {o:bar}")
-	p2.SetOut("bar", ".tmp/bar.txt")
+	p2a := wf.NewProc("p2a", "touch {o:bar}")
+	p2a.SetOut("bar", ".tmp/bar.txt")
+
+	// Create a non-empty file
+	p1b := wf.NewProc("p1b", "echo foz > {o:foz}")
+	p1b.SetOut("foz", ".tmp/foz.txt")
+
+	// Create a non-empty file
+	p2b := wf.NewProc("p2b", "echo baz > {o:baz}")
+	p2b.SetOut("baz", ".tmp/baz.txt")
 
 	// This function should return true for any files to be INCLUDED and false
 	// for any files that should be DROPPED
@@ -26,8 +34,11 @@ func TestIPSelectorSync(t *testing.T) {
 	filterEmpty := NewIPSelectorSync(wf, "filter-empty", filterEmptyFilesFunc)
 	// Note that the in-ports are created on-demand, so you just access them
 	// and use them
-	filterEmpty.In("foo").From(p1.Out("foo"))
-	filterEmpty.In("bar").From(p2.Out("bar"))
+	filterEmpty.In("foo").From(p1a.Out("foo"))
+	filterEmpty.In("bar").From(p2a.Out("bar"))
+
+	filterEmpty.In("foo").From(p1b.Out("foz"))
+	filterEmpty.In("bar").From(p2b.Out("baz"))
 
 	filePadder := wf.NewProc("filepadder", "cat {i:in} > {o:out}")
 	filePadder.SetOut("out", "{i:in}.padded.txt")
@@ -47,10 +58,23 @@ func TestIPSelectorSync(t *testing.T) {
 		}
 	}
 
+	// Check that we DO have the outputs of the first second ones, which are
+	// both non-empty
+	for _, fileName := range []string{".tmp/foz.txt.padded.txt", ".tmp/baz.txt.padded.txt"} {
+		_, err := os.Stat(fileName)
+		if err != nil {
+			t.Errorf("File should exist, and not be filtered out: %s\n", fileName)
+		}
+	}
+
 	cleanFiles([]string{
 		".tmp/foo.txt",
 		".tmp/bar.txt",
+		".tmp/foz.txt",
+		".tmp/baz.txt",
 		".tmp/foo.txt.padded.txt",
 		".tmp/bar.txt.padded.txt",
+		".tmp/foz.txt.padded.txt",
+		".tmp/baz.txt.padded.txt",
 	}...)
 }
